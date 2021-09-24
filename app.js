285ec37e8de76f3de1224e19b18245a9e87ae8d0
Andy Chung <andy6756@gmail.com>
2021-09-24 14:42:32 +0800

    Fail Firebase Database

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-var a = 1;
-var b = 2;
-var c = 3;
-debugger
-a = 4;
-
-b = 6;
-
-debugger
-
-var dogStatus = require('./dogStatus');
-
-dogStatus.bark(4);
+var express = require('express');
+var app = express();
+console.log(app);
