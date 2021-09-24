2f8d077356cdb7ffe69e0f2220f0057e133e73bf
Andy Chung <andy6756@gmail.com>
2021-09-24 14:47:50 +0800

    First Express Application

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 var express = require('express');
 var app = express();
-console.log(app);
+
+var hiHtml = '<html><head></head><body><h1>Hi</h1></body></html>';
+app.get('/', function(req, res){
+    res.send(hiHtml);
+})
+
+//監聽 port
+var port = process.env.PORT || 3000;
+app.listen(port);
+
