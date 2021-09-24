1806087cdd04ec1e88ce5a173aaf1324af723228
Andy Chung <andy6756@gmail.com>
2021-09-24 14:57:17 +0800

    test route rule setting

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get('/', function(req, res){
     res.send(hiHtml);
 })
 
+var userHtml = '<html><head></head><body><h1>Andy</h1></body></html>';
+app.get('/user', function(req, res){
+    res.send(userHtml);
+})
+
 //監聽 port
 var port = process.env.PORT || 3000;
 app.listen(port);
